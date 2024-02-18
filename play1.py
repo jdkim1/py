122c705a4dff19f793467103c2a5465ad77235b4
inflex <isbncd89@gmail.com>
2024-02-18 09:31:58 +0900

    First Commit

---
+++ play1.py
@@ -0,0 +1,27 @@
+import json
+
+user = json.loads('{"id":100, "name":"홍길동"}')
+
+print(user['name'])
+
+def userinfo(u):
+    """사용자 정보를 상세히 보여줌
+
+    Args:
+        u (User): User Json Object
+
+    Returns:
+        String: User Information
+    """
+    id = u["id"]
+    name = u["name"]
+    return f'Id is {id}, {name}'
+
+print(userinfo(user))
+
+
+
+
+
+
+
